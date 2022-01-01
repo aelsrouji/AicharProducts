7ba86f694b7acd2fe5123e0edf462c5d36a80efb
Ayman Elsrouji <ayman4001@hotmail.com>
2022-01-01 10:04:16 -0500

    Adding AutoMapper DependencyInjection NuGet package

--- Services.ShoppingCartAPI/Startup.cs
+++ Services.ShoppingCartAPI/Startup.cs
@@ -1,19 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Services.ShoppingCartAPI.Controllers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Services.ShoppingCartAPI
 {
