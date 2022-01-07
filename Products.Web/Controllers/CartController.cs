ac52255bec012ff8479a80378fd7c447f7d4d622
Ayman Elsrouji <ayman4001@hotmail.com>
2022-01-07 15:44:48 -0500

    Adding remove item from cart

--- Products.Web/Controllers/CartController.cs
+++ Products.Web/Controllers/CartController.cs
@@ -27,6 +27,21 @@ namespace Products.Web.Controllers
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
 
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            //todo: after adding authentication - identity server 
+            //var userId = User.Claims.Where(_ => _.Type == "sub")?.FirstOrDefault()?.Value;
+            //var accessToken = await HttpContext.GetTokenAsync("access_token");
+            //var response = _cartService.GetCartByUserIdAsync<ResponseDto>(userId, accessToken);
+            var response = await _cartService.RemoveFromCartAsync<ResponseDto>(cartDetailsId, null);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
            //todo: after adding authentication - identity server 
